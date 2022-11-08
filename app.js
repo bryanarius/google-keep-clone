9f3e33e8fd6c2658b813574556f0e3fc695b8750
Bryan Arius <98681192+Bryanarius@users.noreply.github.com>
2022-11-07 23:47:32 -0500

    Almost Completed V1
    - Improving functionality
    - Displaying notes in app
    - Editing Notes Capabilities
    - Changing color of notes functionality (not working properly)
    - bug fixes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 class App {
   constructor() {
     this.notes = [];
+    this.title = '';
+    this.text = '';
+    this.id = '';
 
     this.$placeholder = document.querySelector('#placeholder')
     this.$form = document.querySelector('#form');
@@ -9,14 +12,44 @@ class App {
     this.$noteText = document.querySelector('#note-text');
     this.$formButtons = document.querySelector('#form-buttons');
     this.$formCloseButton = document.querySelector('#form-close-button');
+    this.$modal = document.querySelector(".modal");
+    this.$modalTitle = document.querySelector(".modal-title");
+    this.$modalText = document.querySelector(".modal-text");
+    this.$modalCloseButton = document.querySelector('.modal-close-button');
+    this.$colorTooltip = document.querySelector('#color-tooltip');
     
     this.addEventListeners();
   }
   
   addEventListeners() {
     document.body.addEventListener('click', event => {
-      this.handleFormClick(event)
+      this.handleFormClick(event);
+      this.selectNote(event);
+      this.openModal(event);
     });
+
+    document.body.addEventListener('mouseover', event => {
+      this.openTooltip(event);  
+   });
+
+    document.body.addEventListener('mouseout', event => {
+      this.closeTooltip(event);  
+   });
+
+   this.$colorTooltip.addEventListener('mouseover', function(){
+    this.style.display = 'flex';  
+  });
+  
+  this.$colorTooltip.addEventListener('mouseout', function() {
+     this.style.display = 'none'; 
+  });
+
+  this.$colorTooltip.addEventListener('click', event => {
+    const color = event.target.dataset.color; 
+    if (color) {
+      this.editNoteColor(color);  
+    }
+ }) 
 
     this.$form.addEventListener('submit', event => {
       event.preventDefault();
@@ -32,6 +65,10 @@ class App {
     this.$formCloseButton.addEventListener('click' , event => {
       event.stopPropagation();
       this.closeForm();
+    });
+
+    this.$modalCloseButton.addEventListener('click', event => {
+      this.closeModal(event);
     })
   }
 
@@ -64,6 +101,34 @@ class App {
     this.$noteText.value = ''
   }
 
+  openModal(event) {
+    if (event.target.closest('.note')) {
+        this.$modal.classList.toggle('open-modal');
+        this.$modalTitle.value = this.title;
+        this.$modalText.value = this.text;
+    }
+  }
+
+  closeModal(event) {
+      this.editNote();
+      this.$modal.classList.toggle('open-modal')
+  }
+
+  openTooltip(event) {
+    if (!event.target.matches('.toolbar-color')) return;
+    this.id = event.target.dataset.id;
+    const noteCoords = event.target.getBoundingClientRect();
+    const horizontal = noteCoords.left + window.scrollX;
+    const vertical = noteCoords.top + window.scrollY - 20;
+    this.$colorTooltip.style.transform = `translate(${horizontal}px, ${vertical}px)`;
+    this.$colorTooltip.style.display = 'flex';
+  }
+
+  closeTooltip(event) {
+    if (!event.target.matches('.toolbar-color')) return;
+    this.$colorTooltip.style.display = 'none';  
+  }
+
   addNote({title, text}) {
     const newNote = {
       title,
@@ -76,17 +141,42 @@ class App {
     this.closeForm();
   }
 
+  editNote() {
+    const title = this.$modalTitle.value;
+    const text = this.$modalTitle.value;
+    this.notes = this.notes.map(note => 
+        note.id === Number(this.id) ? { ...note, title, text} : note
+      );
+      this.displayNotes()
+  }
+
+  editNoteColor(color) {
+    this.notes = this.notes.map(note =>
+      note.id === Number(this.id) ? { ...note, color } : note
+    );
+    this.displayNotes();
+  }
+
+  selectNote(event) {
+    const $selectedNote = event.target.closest('.note');
+    if (!$selectedNote) return
+    const [$noteTitle, $noteText] = $selectedNote.children;
+    this.title = $noteTitle.innerText;
+    this.text = $noteText.innerText;
+    this.id = $selectedNote.dataset.id;
+  }
+
   displayNotes() {
     const hasNotes = this.notes.length > 0;
     this.$placeholder.style.display = hasNotes ? 'none' : 'flex';
 
     this.$notes.innerHTML = this.notes.map(note => `
-    <div style ="background: ${note.color};" class="note">
+    <div style ="background: ${note.color};" class="note" data-id="${note.id}">
       <div class="${note.title && 'note-title'}">${note.title}</div>
       <div class="note-text">${note.text}</div>
       <div class="toolbar-container">
         <div class="toolbar">
-          <img class="toolbar-color" src="https://icon.now.sh/palette">
+          <img class="toolbar-color" data-id=${note.id} src="https://icon.now.sh/palette">
           <img class="toolbar-delete" src="https://icon.now.sh/delete">
         </div>
       </div>
